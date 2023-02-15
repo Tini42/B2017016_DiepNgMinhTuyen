106b7b166a7b3da3e8f09d81fef2062d7a4394ee
Tini42 <diepnguyenminhtuyen2002@gmail.com>
2023-02-15 13:12:49 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors")
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
 
@@ -10,4 +12,13 @@ app.get("/",(reg,res) => {
     res.json({message: "Welcom to conatct book application."});
 });
 app.use("/api/contacts", contactsRouter);
+//handle response 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 5).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 module.exports = app;
